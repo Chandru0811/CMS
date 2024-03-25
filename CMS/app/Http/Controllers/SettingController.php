0531f31f2e79605861c17b03908ed6f45825d0a0
Chandru0811 <chandru08112000@gmail.com>
2024-03-25 15:47:32 +0530

    Product api added

--- CMS/app/Http/Controllers/SettingController.php
+++ CMS/app/Http/Controllers/SettingController.php
@@ -4,19 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class SettingController extends Controller
 {
 
-    public function show()
+    public function show($id)
     {
-        return response()->json('Hello World');
-    }
+        // Product Detail
+        $settings = Setting::find($id);
+        if (!$settings) {
+            return response()->json([
+                'message' => 'Settings Not Found.'
+            ], 404);
+        }
 
+        // Return Json Response
+        return response()->json([
+            'settings' => $settings
+        ], 200);
+    }
 
     public function update(Request $request, Setting $setting)
     {
-        $setting->update($request->all());
-        return response()->json($setting, 200);
+        try {
+            // Validate the incoming request data (if needed)
+
+            // Handle image uploading
+            if ($request->hasFile('logo')) {
+                $image = $request->file('logo');
+                $imageName = time() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+                // Save the image path to the database
+                $setting->logo = '/images/' . $imageName; // Update the image path based on your storage logic
+            }
+
+            // Update other fields
+            $setting->fill($request->except('logo')); // Exclude logo from the mass assignment
+
+            // Save the updated setting
+            $setting->save();
+
+            // Construct the response message
+            $responseMessage = 'Settings updated successfully';
+
+            return response()->json(['message' => $responseMessage], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            // Handle any unexpected errors
+            return response()->json(['message' => 'An error occurred while updating the setting.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- CMS/routes/api.php
+++ CMS/routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SettingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::get('/settings', [SettingController::class, 'show']);
+Route::get('/settings/{setting}', [SettingController::class, 'show']);
 Route::put('/settings/{setting}', [SettingController::class, 'update']);
+
+// Product
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
+Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
