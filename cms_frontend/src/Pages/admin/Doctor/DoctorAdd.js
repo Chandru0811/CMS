5679d2b5315f16974b9f4ca9230ee8ac605629aa
Sridaran04dec <sri04122001@gmail.com>
2024-03-30 17:55:17 +0530

    Doctor page pending

--- cms_frontend/src/Pages/admin/Doctor/DoctorAdd.js
+++ cms_frontend/src/Pages/admin/Doctor/DoctorAdd.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import profiles from "./Profiles";
 
 function DoctorAdd() {
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [star, setStar] = useState("");
+  const [image, setImage] = useState(null);
+
+  let navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const ids = uuid();
+    let uniqueId = ids.slice(0, 8);
+
+    profiles.push({
+      id: uniqueId,
+      name: name,
+      title: title,
+      stars: star,
+      image: image ? image.name : "",
+    });
+    navigate("/admin/doctor");
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
   return (
-    <div>DoctorAdd</div>
-  )
+    <div>
+      <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formStars">
+          <Form.Label>Stars</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Rating"
+            required
+            value={star}
+            onChange={(e) => setStar(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImage">
+          <Form.Label>Image</Form.Label>
+          <Form.Control
+            type="file"
+            accept="image"
+            onChange={handleImageChange}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default DoctorAdd
+export default DoctorAdd;
--- cms_frontend/src/Pages/admin/Doctor/DoctorEdit.js
+++ cms_frontend/src/Pages/admin/Doctor/DoctorEdit.js
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import profiles from "./Profiles";
 
 function DoctorEdit() {
-  return <div>DoctorEdit</div>;
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [star, setStar] = useState("");
+  const [image, setImage] = useState(null);
+  const [id, setId] = useState("");
+
+  let navigate = useNavigate();
+
+  var index = profiles
+    .map(function (e) {
+      return e.id;
+    })
+    .indexOf(id);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let updatedProfile = { ...profiles[index] };
+    updatedProfile.name = name;
+    updatedProfile.title = title;
+    updatedProfile.stars = star;
+    if (image) {
+      updatedProfile.image = image.name;
+    }
+
+    profiles[index] = updatedProfile;
+
+    navigate("/admin/doctor");
+  };
+
+  useEffect(() => {
+    setName(localStorage.getItem("name"));
+    setTitle(localStorage.getItem("title"));
+    setStar(localStorage.getItem("stars"));
+    setId(localStorage.getItem("id"));
+  }, []);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  return (
+    <div>
+      <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
+        <Form.Group className="mb-3" controlId="formImage">
+          <Form.Label>Image</Form.Label>
+          <Form.Control
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formStars">
+          <Form.Label>Stars</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Rating"
+            required
+            value={star}
+            onChange={(e) => setStar(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Update
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default DoctorEdit;
