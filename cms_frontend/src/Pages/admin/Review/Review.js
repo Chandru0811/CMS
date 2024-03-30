c85af6a7f8ffad5990ca79c007ccf7f4c8e9530a
Chandru0811 <chandru08112000@gmail.com>
2024-03-30 18:21:44 +0530

    review added

--- cms_frontend/src/Pages/admin/Review/Review.js
+++ cms_frontend/src/Pages/admin/Review/Review.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import ReviewAdd from "./ReviewAdd";
+import { MdDelete } from "react-icons/md";
 
 function Review() {
   return (
-    <div>Review</div>
-  )
+    <section className="container my-4">
+      <div className="card shadow border-0 mb-2 top-header">
+        <div className="container-fluid py-4">
+          <div className="row align-items-center justify-content-between ">
+            <div className="col">
+              <div className="d-flex align-items-center gap-4">
+                <h1 className="h4 ls-tight">Review</h1>
+              </div>
+            </div>
+            <div className="col-auto">
+              <div className="hstack gap-2 justify-content-end">
+                <ReviewAdd />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card shadow border-0 mb-2 top-header">
+        <div className="container py-4">
+        <div className="row g-3">
+          <div className="table-responsive p-2">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Review Content</th>
+                  <th scope="col">Review Name</th>
+                  <th scope="col">Review Address</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Singapore</td>
+                  <td>
+                    <Link to="/">
+                      <button className="btn btn-sm shadow-none border-none">
+                        <MdDelete />
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Review
+export default Review;
--- cms_frontend/src/Pages/admin/Review/ReviewAdd.js
+++ cms_frontend/src/Pages/admin/Review/ReviewAdd.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function ReviewAdd() {
+  const validationSchema = Yup.object({
+    reviewName: Yup.string().required("*Review Name is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      reviewName: "",
+      address: "",
+      comments: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>ReviewAdd</div>
-  )
+    <>
+      <button
+        onClick={handleShow}
+        type="button"
+        className="btn text-appointment-btn rounded-4 fs-6"
+      >
+        Add
+      </button>
+
+      <Modal size="lg" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Associate Job Opening</Modal.Title>
+        </Modal.Header>
+        <form onSubmit={formik.handleSubmit}>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-md-12 col-12 mb-2">
+                <label className="form-label">Review Content</label>
+                <div className="mb-3">
+                  <div className="form-floating">
+                    <textarea
+                      rows="5"
+                      className="form-control"
+                      {...formik.getFieldProps("content")}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-12 mb-2">
+                <label className="form-label">Review Address</label>
+                <div className="mb-3">
+                  <div className="form-floating">
+                    <textarea
+                      className="form-control"
+                      {...formik.getFieldProps("address")}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-12 mb-2">
+                <lable className="form-lable">
+                  Review Name<span className="text-danger">*</span>
+                </lable>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    name="reviewName"
+                    className={`form-control  ${
+                      formik.touched.reviewName && formik.errors.reviewName
+                        ? "is-invalid"
+                        : ""
+                    }`}
+                    {...formik.getFieldProps("reviewName")}
+                  />
+                  {formik.touched.reviewName && formik.errors.reviewName && (
+                    <div className="invalid-feedback">
+                      {formik.errors.reviewName}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              className="btn btn-secondary"
+              type="button"
+              onClick={handleClose}
+            >
+              Close
+            </button>
+            <button type="submit" className="btn btn-danger">
+              Save
+            </button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </>
+  );
 }
 
-export default ReviewAdd
+export default ReviewAdd;
